1665750bd9b6916fcfab24e7a34ad461ec5e0196
Ryan Moore <rhcmoore@gmail.com>
2019-02-18 16:18:08 -0800

    DEPLOYMENT IS WORKING

---
+++ database/seeds.sql
@@ -0,0 +1,1 @@
+INSERT INTO Items (name, description, image, price, location, sellerid, buyerid, reservetime, createdAt, updatedAt) VALUES ("Ryan's socks", "V smelly", "https://cdn11.bigcommerce.com/s-80dc88lad1/images/stencil/1024x1024/products/130/1287/Rubys_Rainbow_Pride_Socks_Down_syndrome__33484.1536546296.jpg", 8, "SF", 1, 2, 10, "2019-01-28 09:00:00", "2019-01-28 09:00:00");
